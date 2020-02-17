0e367bfed01ec03ea316b59e5cc4a402b402c453
mtzo09-dev <meflores.1989@gmail.com>
2020-02-17 14:31:47 -0300

    Updates

---
+++ precache-manifest.5d5be2dd2c9f8ab3ee32264664340360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b62c156a525d8fb7acde8059ef0f79",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "8adf517de6af73d8812f",
+    "url": "/jammming/static/css/main.7dd28965.chunk.css"
+  },
+  {
+    "revision": "85f0bc241e3ca3780b69",
+    "url": "/jammming/static/js/2.fc04184f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/jammming/static/js/2.fc04184f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adf517de6af73d8812f",
+    "url": "/jammming/static/js/main.b884c94d.chunk.js"
+  },
+  {
+    "revision": "3cead86a956098c6f441",
+    "url": "/jammming/static/js/runtime-main.3a5ccc2d.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
